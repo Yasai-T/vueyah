056575eefd700d92e506ab71bba68037fdf43529
tomoki midorikawa <46930312+Yasai-T@users.noreply.github.com>
2020-01-23 22:46:52 +0900

    feat: text input

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -2,12 +2,49 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "HelloWorld",
-  props: {
-    msg: String
+  data() {
+    return {
+      input: ""
+    };
+  },
+  methods: {
+    keydown(e: KeyboardEvent) {
+      console.log(e);
+      if (
+        e.metaKey ||
+        e.altKey ||
+        e.ctrlKey ||
+        (e.shiftKey && e.key === "Shift")
+      ) {
+        return;
+      }
+      switch (e.code) {
+        case "Space":
+          this.input += " "; // TODO: Space handling
+          break;
+        case "Enter":
+          this.input += "\n";
+          break;
+        case "Backspace":
+          this.input = this.input.slice(0, -1);
+          break;
+        case "Tab":
+          this.input += "    ";
+          break;
+        default:
+          this.input += e.key;
+          console.log("input", this.input);
+          break;
+      }
+    }
   },
   render() {
+    document.onkeydown = this.keydown;
     return (
-      <div>hellooo</div>
-    )
+      <div>
+        <div>Type KeyBoard!!!!!</div>
+        <div>{this.input}</div>
+      </div>
+    );
   }
-})
+});
